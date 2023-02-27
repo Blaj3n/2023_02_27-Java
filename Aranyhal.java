c22b5d758086fdb7cf302914cff3496f8cdd2da1
Blaj3n <95749747+Blaj3n@users.noreply.github.com>
2023-02-27 12:46:48 +0100

    metódusok, getterek, setterek

---
+++ Aranyhal.java
@@ -0,0 +1,56 @@
+package aranyhal;
+
+public class Aranyhal {
+
+    private final int kivansagokszama = 3;
+    private int kor;
+    private String kifogta;
+
+    public static void main(String[] args) {
+
+    }
+
+    public String Aranyhal(String kifogta) {
+
+        return null;
+
+    }
+
+    public String Aranyhal(String kifogta, int kor) {
+
+        return null;
+
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKifogta(String kifogta) {
+        this.kifogta = kifogta;
+    }
+
+    public int kivansagokSzama(String kifogta) {
+
+        return 0;
+
+    }
+
+    public String[] kikkelTalalkozott() {
+
+        return null;
+
+    }
+
+    public boolean kivansagotTeljesit(String kivansag) {
+
+        return false;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Aranyhal{" + "kifogta=" + kifogta + '}';
+    }
+
+}
